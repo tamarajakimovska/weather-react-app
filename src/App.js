05dd7afb7e6db7a7aa3e24e22c6269db62089f4e
tamarajakimovska <tamarajakimovska007@gmail.com>
2023-06-27 13:10:33 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,24 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className=" App">
-      <h1> Weather App </h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/tamara-jakimovska-869211240/"
-          target="_blank"
-        >
-          {" "}
-          Tamara Jakimovska{" "}
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/tamarajakimovska/weather-react-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App </h1>
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/tamara-jakimovska-869211240/"
+            target="_blank"
+          >
+            {" "}
+            Tamara Jakimovska{" "}
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/tamarajakimovska/weather-react-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
