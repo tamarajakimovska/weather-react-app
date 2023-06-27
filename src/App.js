7c57e26308a6e90dbb84b3136affd7efbb2779e0
tamarajakimovska <tamarajakimovska007@gmail.com>
2023-06-27 12:34:16 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,26 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="Weather App">Weather App</div>;
+  return (
+    <div className=" App">
+      <h1> Weather App </h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/tamara-jakimovska-869211240/"
+          target="_blank"
+        >
+          {" "}
+          Tamara Jakimovska{" "}
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/tamarajakimovska/weather-react-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
